5f3afffd000fdb2a2c5e8c42fa322b456ec217bd
Ralph Delia <Ralphdelia1@gmail.com>
2025-01-18 13:23:24 -0500

    filling out routes

--- backend/src/routes/requestRoutes.ts
+++ backend/src/routes/requestRoutes.ts
@@ -1,15 +1,36 @@
 import express from "express";
+import { join } from "node:path/win32";
+// import pgQuery from "../db/models/postgresModel";
 
 const requestRouter = express.Router();
 
-requestRouter.all("/:id", (req, res) => {
-  const headers = req.headers;
-  const method = req.method;
-  const body = req.body;
+requestRouter.all("/:id", async (req, res) => {
+  const uuid = req.params.id;
+  try {
+    const bucketRow = await pgQuery("SELECT id FROM bucket WHERE uuid = 1$", [
+      uuid,
+    ]);
 
-  console.log(method, headers, body);
+    if (!bucketRow) {
+      res.status(404);
+      return;
+    }
 
-  res.send(200);
+    const body = req.body;
+    // add body
+    let mongoID; // =  add body to mongo recieve id
+    const headers = req.headers;
+    const method = req.method;
+    const result = await pgQuery(
+      `INSERT INTO TABLE requests (bucket_id, method, headers, mongo_id)
+      values (1$, 2$, 3$, 4$);`,
+      [bucketRow, method, headers, mongoID],
+    );
+
+    res.send(200);
+  } catch (e: unknown) {
+    res.send(500);
+  }
 });
 
 export default requestRouter;
