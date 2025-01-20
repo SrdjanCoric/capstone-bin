3163ddba6c2e91a0f3ec508822096cb4ce0dded6
Mykel Murphy <murphy@akane.waseda.jp>
2025-01-20 09:39:04 -0500

    start modal, delete bucketlinks and some comments

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,11 +5,8 @@ const Header = () => {
       <Link to={"/"}>
         <div className="title-bar-text">Request Buckets NY</div>
       </Link>
-      {/* <div className="title-bar-controls"></div> */}
     </div>
   );
 };
 
-// link to always go to home page in the left corner
-//
 export default Header;
--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -1,7 +1,14 @@
+// import { useState } from "react";
 import CreateBucket from "./CreateBucket";
 import Sidebar from "./Sidebar";
 
 const HomePage = () => {
+  // const [modalVisible, setModalVisible] = useState(false);
+
+  // const toggleModal = () => {
+  //   setModalVisible(!modalVisible);
+  // };
+
   return (
     <div className="homepage">
       <CreateBucket />
